66972276c76a4a42dab48440d9a436c3978a9886
Barogthor <f.thery3103@gmail.com>
2022-12-09 16:07:22 +0100

    Day1 - Diner philosopher parameters

--- FirstWeek/src/main/java/org/example/Philosopher.java
+++ FirstWeek/src/main/java/org/example/Philosopher.java
@@ -13,12 +13,16 @@ import java.util.List;
 import java.util.Random;
 
 class Philosopher extends Thread {
+  private final int maxSecondsEatingTime;
+  private final int maxSecondsThinkingTime;
   private Chopstick left, right;
   private Random random;
   private int thinkCount;
 
-  public Philosopher(Chopstick left, Chopstick right) {
+  public Philosopher(Chopstick left, Chopstick right, int maxSecondsEatingTime, int maxSecondsThinkingTime) {
     this.left = left; this.right = right;
+    this.maxSecondsEatingTime = maxSecondsEatingTime * 1000;
+    this.maxSecondsThinkingTime = maxSecondsThinkingTime * 1000;
     random = new Random();
   }
 
@@ -28,10 +32,14 @@ class Philosopher extends Thread {
         ++thinkCount;
         if (thinkCount % 10 == 0)
           System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+        System.out.println(this + " is thinking.");
+        Thread.sleep(random.nextInt(this.maxSecondsThinkingTime));     // Think for a while
+        System.out.println(this + " try to takes chopstick left " + left.getId());
+        synchronized(left) {                    // Grab left chopstick
+          System.out.println(this + " try to takes chopstick right " + right.getId());
+          synchronized(right) {                 // Grab right chopstick
+            System.out.println(this + " is eating.");
+            Thread.sleep(random.nextInt(this.maxSecondsEatingTime)); // Eat for a while
           }
         }
       }
@@ -39,15 +47,16 @@ class Philosopher extends Thread {
   }
 
   public static void main(String[] args) throws InterruptedException {
+    final int number = 30;
     List<Chopstick> chopsticks = new ArrayList<>();
     List<Thread> philosophers = new ArrayList<>();
-    for(int i = 0 ; i < 5 ; i++) {
+    for(int i = 0 ; i < number ; i++) {
       chopsticks.add(new Chopstick(i));
     }
-    for(int i = 0 ; i < 5 ; i++) {
+    for(int i = 0 ; i < number ; i++) {
       int leftId = i;
-      int rightId = (i+1)%5;
-      Philosopher e = new Philosopher(chopsticks.get(leftId), chopsticks.get(rightId));
+      int rightId = (i+1)%number;
+      Philosopher e = new Philosopher(chopsticks.get(leftId), chopsticks.get(rightId), 10, 5);
       philosophers.add(e);
       e.start();
     }
